6356c2dadb4820e08b52522482144948fe50cb8b
zeuscm <zeuscm.work@gmail.com>
2024-10-05 17:18:09 -0600

    FIX - MODELO USUARIOS

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,15 +5,17 @@ class UsuariosManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
         if not username:
             raise ValueError('El campo username es obligatorio')
+        if not password:
+            raise ValueError('El campo password es obligatorio')
+
         user = self.model(username=username, **extra_fields)
-        user.set_password(password)
+        user.set_password(password)  # Establece el password utilizando hashing
         user.save(using=self._db)
         return user
 
     def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
+        extra_fields.setdefault('is_staff', True)  # Establece que es superusuario
+        extra_fields.setdefault('is_superuser', True)  # Establece que es superusuario
         return self.create_user(username, password, **extra_fields)
 
 class Usuarios(AbstractBaseUser, PermissionsMixin):
@@ -23,20 +25,15 @@ class Usuarios(AbstractBaseUser, PermissionsMixin):
     apellido_paterno = models.CharField(max_length=20)
     apellido_materno = models.CharField(max_length=20)
     correo = models.EmailField(max_length=50, unique=True)
-    activo = models.BooleanField(default=True)
+    password = models.CharField(max_length=500)
+    pista_password = models.CharField(max_length=80)
+    activo = models.BooleanField(null=True)  # Puedes cambiar a default=True si todos los usuarios son activos por defecto
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     ultimo_acceso = models.DateTimeField(null=True, blank=True)
 
-    # Campos requeridos para usar AbstractBaseUser
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
     objects = UsuariosManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['nombres', 'apellido_paterno', 'apellido_materno', 'correo']
-
-    # Añadir estos campos para evitar conflictos
+    # Campos para relaciones con grupos y permisos
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='usuarios_set',  # Cambiar el nombre del grupo relacionado
@@ -47,6 +44,9 @@ class Usuarios(AbstractBaseUser, PermissionsMixin):
         related_name='usuarios_set',  # Cambiar el nombre del permiso relacionado
         blank=True,
     )
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['nombres', 'apellido_paterno', 'apellido_materno', 'correo', 'pista_password']
 
     class Meta:
         db_table = 'usuarios'
